3d94233ae7256cee5d46e36eba8965c8da71677f
Craig Bass <craig@clearbooks.co.uk>
2015-07-13 14:53:31 +0100

    Fix issues with the PHPUnit configuration

---
+++ src/Segment/GetAllSegments.php
@@ -0,0 +1,7 @@
+<?php
+namespace Clearbooks\Labs\Segment;
+
+class GetAllSegments 
+{
+
+}
---
+++ test/Segment/GetAllSegmentsTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Clearbooks\Labs\Segment;
+
+class GetAllSegmentsTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function canConstruct() {
+        new GetAllSegments();
+    }
+
+}
